5f9d38ac79b34a5e781268b064f2b62fb2652616
Daniel Shoemaker <danielshoe@charter.net>
2021-09-24 19:19:45 -0700

    Initial Commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,11 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        String name = "Daniel";
+        int age = 48;
+        String homeTown = "Apple Valley";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Home Town: " + homeTown);
+    }
+}
